0d32716cff7a8d24bf6792501fb7c2e6deb06ee9
Tom <tomshinton@googlemail.com>
2020-11-30 01:00:47 +0000

    Initialising ObjectMessaging Plugin for Unreal Engine

--- Source/ObjectMessaging/Public/Sender/ObjectMessagingFunctions.h
+++ Source/ObjectMessaging/Public/Sender/ObjectMessagingFunctions.h
@@ -6,6 +6,8 @@
 
 #include <Runtime/CoreUObject/Public/UObject/WeakInterfacePtr.h>
 
+DEFINE_LOG_CATEGORY_STATIC(ObjectMessagingFunctionsLog, Log, Log)
+
 //-----------------------------------------------------------------------------------------------
 
 namespace ObjectMessagingFunctions
@@ -15,6 +17,19 @@ namespace ObjectMessagingFunctions
 	{
 		InRecipient->GetListener().InvokeNewEvent<TEvent>(InEvent);
 	};
+
+	template<typename TEvent>
+	static void SendMessage(UObject& InRecievingObject, const TEvent& InEvent)
+	{
+		if (IObjectMessagingListenerInterface* ObjectMessagingListenerInterface = Cast<IObjectMessagingListenerInterface>(&InRecievingObject))
+		{
+			ObjectMessagingListenerInterface->GetListener().InvokeNewEvent<TEvent>(InEvent);
+		}
+		else
+		{
+			UE_LOG(ObjectMessagingFunctionsLog, Warning, TEXT("%s does not implement an Object Message Listener - cannot send message"), *InRecievingObject.GetName());
+		}
+	}
 }
 
 //-----------------------------------------------------------------------------------------------
