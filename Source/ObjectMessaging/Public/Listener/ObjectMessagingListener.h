7ddcc0f5eaf5f4342947f3647d0dd11615f389f5
Matt Atkinson <awesumson@me.com>
2023-09-30 22:08:27 +0100

    Changed Bindings to a TMap to have an FGuid Key

--- Source/ObjectMessaging/Public/Listener/ObjectMessagingListener.h
+++ Source/ObjectMessaging/Public/Listener/ObjectMessagingListener.h
@@ -73,47 +73,47 @@ public:
 	};
 
 	template<typename TEvent>
-	TSharedPtr<FBindingBase> Bind(const TFunction<void(const TEvent&)>& InCallback)
+	FGuid Bind(const TFunction<void(const TEvent&)>& InCallback)
 	{
-		TSharedPtr<FBindingBase> NewBinding = MakeShareable(new FBinding<TEvent>(InCallback));
-		Bindings.Add(NewBinding);
-		return NewBinding;
+		FGuid BindingID = FGuid::NewGuid();
+		Bindings.Add(BindingID, MakeShareable(new FBinding<TEvent>(InCallback)));
+		return BindingID;
 	}
 
-	void Unbind(TSharedPtr<FBindingBase> InBinding)
+	void Unbind(FGuid InBindingID)
 	{
-		Bindings.Remove(InBinding);
+		Bindings.Remove(InBindingID);
 	}
 
 	template<typename TEvent>
 	void InvokeNewEvent(const TEvent& InEvent)
 	{
+		TArray<FGuid> BindingIDs;
+		Bindings.GenerateKeyArray(BindingIDs);
+
 		for (int32 Index = 0; Index < Bindings.Num(); ++Index)
 		{
-			if (Bindings.IsValidIndex(Index))
+			TSharedPtr<FBindingBase> Binding = *Bindings.Find(BindingIDs[Index]);
+			if (Binding.IsValid())
 			{
-				TSharedPtr<FBindingBase> Binding = Bindings[Index];
-				if (Binding.IsValid())
+				if (Binding->ShouldExecute(*TEvent::StaticStruct()))
 				{
-					if (Binding->ShouldExecute(*TEvent::StaticStruct()))
+					if (const void* DataRaw = &InEvent)
 					{
-						if (const void* DataRaw = &InEvent)
-						{
-							Binding->Execute(DataRaw);
-						}
+						Binding->Execute(DataRaw);
 					}
 				}
-				else
-				{
-					UE_LOG(ObjectMessagingListenerLog, Error, TEXT("Could not invoke function - binding is invalid!"));
-				}
+			}
+			else
+			{
+				UE_LOG(ObjectMessagingListenerLog, Error, TEXT("Could not invoke function - binding is invalid!"));
 			}
 		}
 	}
 
 private:
 
-	TArray<TSharedPtr<FBindingBase>> Bindings;
+	TMap<FGuid, TSharedPtr<FBindingBase>> Bindings;
 };
 
 //-----------------------------------------------------------------------------------------------
